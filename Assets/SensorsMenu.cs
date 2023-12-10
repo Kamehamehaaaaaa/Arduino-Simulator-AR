09a489e2646e298fe7b5fd08cd4db57018fd4297
Kamehamehaaaaaa <sushantsamani@gmail.com>
2023-12-10 20:32:52 +0530

    initial draft commit

---
+++ Assets/SensorsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SensorsMenu : MonoBehaviour
+{
+    public void PlayTemp()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PlayUltrasonic()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void PlaySpeed()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void BackScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
